7a99a8adf38f43c9f9a867f35697c6666214f04e
VexaAuth <anshthedev59@gmail.com>
2026-03-06 11:58:21 +0530

    Rename to botliysdk

--- botliy.py/setup.py
+++ botliy.py/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="botliy",
+    name="botliysdk",
     version="1.0.0",
     author="Botliy",
     description="The official API SDK for botliy.online",
